d20839b0ce105f734f74770fe1f90468cf65982b
Sasha <murzina93@gmail.com>
2015-04-29 23:44:57 +0600

    modified server.py

--- server.py
+++ server.py
@@ -7,6 +7,12 @@ sock.listen(10)
 try:
     while True:
         conn, addr = sock.accept()
+        print "connected"
 
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+        conn.close()
 finally:
     sock.close()
