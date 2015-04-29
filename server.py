3ba3a34356c7fcb596346b73399e25cff692feaa
Sasha <murzina93@gmail.com>
2015-04-30 00:44:18 +0600

    adding asynchronous

--- server.py
+++ server.py
@@ -1,18 +1,33 @@
 import socket
+import asyncore
+import sys
 
-sock = socket.socket()
-sock.bind(('',31337))
-sock.listen(10)
+#sock = socket.socket()
+#sock.bind(('',31337))
+#sock.listen(10)
 
-try:
-    while True:
-        conn, addr = sock.accept()
-        print "connected"
+#try:
+#    while True:
+#        conn, addr = sock.accept()
+#        print "connected"
+#
+#       while True:
+#            data = conn.recv(1024)
+#            if not data:
+#                break
+#        conn.close()
+#finally:
+#    sock.close()
 
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-        conn.close()
-finally:
-    sock.close()
+
+
+
+def main():
+    try:
+        asyncore.loop()
+    except:
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
