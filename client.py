671b560839020571ef8d1c7490dca59730854d51
Sasha <murzina93@gmail.com>
2015-04-30 08:23:17 +0600

    modified client.py

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
 import socket, sys, select
 
+
+def prompt():
+    sys.stdout.write('You: ')
+    sys.stdout.flush()
+
+
 host = "127.0.0.1"
 port = 31337
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(2)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(2)
 
 try:
-    s.connect((host, port))
+    sock.connect((host, port))
 except:
     print 'Something goes wrong'
     sys.exit()
@@ -15,7 +21,20 @@ except:
 print 'Lets get started sending messages :)'
 
 while 1:
-    msg = sys.stdin.readline()
-    s.send(msg)
+    socket_list = [sys.stdin, sock]
+    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
 
-    
+    for s in read_sockets:
+        if s == sock:
+            data = s.recv(4096)
+            if not data:
+                print '\nDisconnected'
+                sys.exit()
+            else:
+                sys.stdout.write(data)
+                prompt()
+
+        else:
+            msg = sys.stdin.readline()
+            sock.send(msg)
+            prompt()
